e95c957c76d1e05cd98df48b9a8a13a0148f4da5
Carlos Montoya <dikarey@gmail.com>
2019-05-30 22:23:22 -0500

    [GRAL] CRUD and Theme

--- Siri_Web_Application/App_Start/BundleConfig.cs
+++ Siri_Web_Application/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Siri_Web_Application
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-cerulean.css",
                       "~/Content/site.css"));
         }
     }
